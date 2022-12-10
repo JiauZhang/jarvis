441e7f3aa74caf25c87e9e09fb9bd572144bd433
Mr. Zhang <38578095+JiauZhang@users.noreply.github.com>
2022-12-10 10:58:21 +0800

    update env (#1)

---
+++ main.py
@@ -0,0 +1,9 @@
+import whisper
+from speechbrain.pretrained import Tacotron2
+from speechbrain.pretrained import HIFIGAN
+
+model = whisper.load_model("tiny.en")
+# Intialize TTS (tacotron2) and Vocoder (HiFIGAN)
+tacotron2 = Tacotron2.from_hparams(source="speechbrain/tts-tacotron2-ljspeech", savedir="tmpdir_tts")
+hifi_gan = HIFIGAN.from_hparams(source="speechbrain/tts-hifigan-ljspeech", savedir="tmpdir_vocoder")
+
